2a159296507eb0f8ffc21f0759060df84d2f634d
adriano-stacks <adriano-stacks@users.noreply.github.com>
2026-08-10 12:12:22 +0200

    Drop `presentation`

--- presentation/tools/loc.nix
+++
@@ -1,9 +0,0 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ../../flake.lock);
-  nixpkgs = builtins.fetchTree lock.nodes.nixpkgs.locked;
-  pkgs = import nixpkgs { system = builtins.currentSystem; };
-in
-pkgs.python313.withPackages (python: [
-  python.tree-sitter
-  python.tree-sitter-rust
-])
